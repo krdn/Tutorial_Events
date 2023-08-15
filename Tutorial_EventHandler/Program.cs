e1ffdc9d30700eaf654dcca0c621040f2f30b4d7
Gon <krdn.net@gmail.com>
2023-08-15 15:02:12 +0900

    초기 커밋.

---
+++ Tutorial_EventHandler/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Tutorial_MulticastDelegate;
+
+
+/// <summary>
+/// 멀티캐스트(delegate) 델리게이트는 C#에서 한 개 이상의 메서드를 동시에 호출
+/// https://www.notion.so/krdn/Delegate-f405a171a41e462ca433bc2bd9bccba9
+/// </summary>
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        MyPublisher publisher = new MyPublisher();
+        MySubscriber subscriber = new MySubscriber();
+
+        publisher.MyEvent += subscriber.OnMyEvent;
+
+        publisher.PublishEvent("Hello from the event!");
+
+        Console.ReadLine();
+    }
+}
+
+public class MyEventArgs : EventArgs
+{
+    public string Message { get; }
+
+    public MyEventArgs(string message)
+    {
+        Message = message;
+    }
+}
+
+public class MyPublisher
+{
+    public event EventHandler<MyEventArgs> MyEvent;
+
+    public void PublishEvent(string message)
+    {
+        MyEvent?.Invoke(this, new MyEventArgs(message));
+    }
+}
+
+public class MySubscriber
+{
+    public void OnMyEvent(object sender, MyEventArgs e)
+    {
+        Console.WriteLine($"Received event: {e.Message}");
+    }
+}
---
+++ Tutorial_MulticastDelegate/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Tutorial_MulticastDelegate;
+
+
+/// <summary>
+/// 멀티캐스트(delegate) 델리게이트는 C#에서 한 개 이상의 메서드를 동시에 호출
+/// https://www.notion.so/krdn/Delegate-f405a171a41e462ca433bc2bd9bccba9
+/// </summary>
+internal class Program
+{
+    public delegate void MyDelegate(string message);
+
+    static void Main(string[] args)
+    {
+        
+        MyDelegate myDelegate = Method1;
+        myDelegate += Method2;
+        myDelegate += Method3;
+
+        myDelegate("Hello, multicast delegates!");
+
+        Console.ReadLine();
+    }
+
+    static void Method1(string message)
+    {
+        Console.WriteLine("Method 1: " + message);
+    }
+
+    static void Method2(string message)
+    {
+        Console.WriteLine("Method 2: " + message);
+    }
+
+    static void Method3(string message)
+    {
+        Console.WriteLine("Method 3: " + message);
+    }
+}
